29540cc586c2643f7fd5f52c51c908133c66cd51
Goraf <22941576+Goraf@users.noreply.github.com>
2017-08-30 14:37:25 +0200

    State: Add enter/exit methods

--- src/State.h
+++ src/State.h
@@ -10,6 +10,9 @@ class State {
 public:
     State(sf::RenderWindow& window) : window(window) { }
 
+    virtual void onEnter() { }
+    virtual void onExit() { }
+
     virtual void handleEvents(const sf::Event& event) = 0;
     virtual void update(const float& deltaTime) = 0;
     virtual void render() = 0;
